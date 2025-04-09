2b8fe3d6cc1e2b07e63235cb9470362dee49d900
Alex198982 <290825279@qq.com>
2025-04-09 20:27:15 +0800

    Update vless源码.js

--- Vless_workers_pages/vless源码.js
+++ Vless_workers_pages/vless源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "49204579-664b-41ef-a0f1-fb80d0c0b06b";
 
 const proxyIPs = ["ts.hpc.tw"]; 
 const cn_hostnames = [''];
